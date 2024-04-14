87628e60f059dde59fe79d62e714fa5d29850e66
Soda7592 <e80704580@gmail.com>
2024-04-14 21:28:40 +0800

    nearby and near threes areas.

--- GameAct.py
+++ GameAct.py
@@ -28,6 +28,51 @@ def Scan(area):
     return f"There is a {StarsMap[MapSet.map[area]]} in area {area}."
 
 
+# 相鄰、正對、幾格之內
+# 彗星、小行星、矮行星、星際雲
+# O 行星(不)與 X 行星正對
+# 至少有一個 O 行星(不)與 X 行星相鄰 (V)
+# O 行星與 X 行星在2~3格以內 (V)
+
+ReasearchString = [[], [], [], [], []]
+
+
+def indexs(planet):
+    index = []
+    for i in range(len(MapSet.map)):
+        if MapSet.map[i] == planet:
+            index.append(i)
+    return index
+
+
+def nearBy(planet):
+    PlanetIndex = indexs(planet)
+    Stringindex = 0
+    p = "1234".replace(str(planet), "")
+    for j in range(3):
+        for i in PlanetIndex:
+            if MapSet.map[(i + 1) % 12] == int(p[j]) or MapSet.map[(i - 1) % 12] == int(
+                p[j]
+            ):
+                ReasearchString[planet].append(
+                    f"At least a {StarsMap[planet]} is nearby a {StarsMap[int(p[j])]}"
+                )
+
+
+def nearBy3(planet):
+    PlanetIndex = indexs(planet)
+    Stringindex = 0
+    p = "1234".replace(str(planet), "")
+    for j in range(3):
+        for i in PlanetIndex:
+            if MapSet.map[(i + 3) % 12] == int(p[j]) or MapSet.map[(i - 3) % 12] == int(
+                p[j]
+            ):
+                ReasearchString[planet].append(
+                    f"There is at least a {StarsMap[planet]} is within three areas of a {StarsMap[int(p[j])]}"
+                )
+
+
 def Research():
     pass
 
@@ -49,38 +94,42 @@ def PointOutX(index, LeftNear, RightNear):
         return False
 
 
-while True:
-    print(
-        " - 1 => Survey (start, end, planet)\n",
-        "- 2 => Scan (area)\n",
-        "- 3 => Research (Couldn't work correctly now.)\n",
-        "- 4 => SubmitPaper (area, planet)\n",
-        "- 5 => PointOutX (AreaOfX, LeftNearPlanet, RightNearPlaner)\n",
-        "- 6 => Help\n",
-        "- 7 => ExitAndGetResult\n",
-    )
-    m = input("Input a Action > ")
-    if m == "1":
-        start = int(input("Start :"))
-        end = int(input("End :"))
-        planet = int(input("Planet : "))
-        print(Survey(start, end, planet))
-    elif m == "2":
-        area = int(input("Area : "))
-        print(Scan(area))
-    elif m == "3":
-        Research()
-    elif m == "4":
-        area = int(input("Area : "))
-        planet = int(input("Planet : "))
-        print(SubmitPaper(area, planet))
-    elif m == "5":
-        index = int(input("AreaOfX :"))
-        LeftNear = int(input("LeftNearPlanet :"))
-        RightNear = int(input("RightNearPlanet :"))
-        print(PointOutX(index, LeftNear, RightNear))
-    elif m == "6":
-        print("Usage Example :")
-    elif m == "7":
-        print(MapSet.map)
-    print("\n")
+if __name__ == "__main__":
+    nearBy(1)
+    nearBy3(1)
+    print(ReasearchString)
+    # while True:
+    #     print(
+    #         " - 1 => Survey (start, end, planet)\n",
+    #         "- 2 => Scan (area)\n",
+    #         "- 3 => Research (Couldn't work correctly now.)\n",
+    #         "- 4 => SubmitPaper (area, planet)\n",
+    #         "- 5 => PointOutX (AreaOfX, LeftNearPlanet, RightNearPlaner)\n",
+    #         "- 6 => Help\n",
+    #         "- 7 => ExitAndGetResult\n",
+    #     )
+    #     m = input("Input a Action > ")
+    #     if m == "1":
+    #         start = int(input("Start :"))
+    #         end = int(input("End :"))
+    #         planet = int(input("Planet : "))
+    #         print(Survey(start, end, planet))
+    #     elif m == "2":
+    #         area = int(input("Area : "))
+    #         print(Scan(area))
+    #     elif m == "3":
+    #         Research()
+    #     elif m == "4":
+    #         area = int(input("Area : "))
+    #         planet = int(input("Planet : "))
+    #         print(SubmitPaper(area, planet))
+    #     elif m == "5":
+    #         index = int(input("AreaOfX :"))
+    #         LeftNear = int(input("LeftNearPlanet :"))
+    #         RightNear = int(input("RightNearPlanet :"))
+    #         print(PointOutX(index, LeftNear, RightNear))
+    #     elif m == "6":
+    #         print("Usage Example :")
+    #     elif m == "7":
+    #         print(MapSet.map)
+    #     print("\n")
